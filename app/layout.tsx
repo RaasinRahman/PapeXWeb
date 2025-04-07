730e1a7fb86890e694a0f0534db2ef917bd0f9d7
Raasin Rahman <raasinr@gmail.com>
2025-04-07 16:54:58 -0400

    Fix favicon to correctly show PapeX logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,14 +5,20 @@ import { barlow, gloock } from './fonts'
 export const metadata: Metadata = {
   title: 'PapeX',
   description: 'Digital Receipts Revolutionized',
-  generator: 'v0.dev',
+  generator: 'Next.js',
   icons: {
     icon: [
-      { url: '/favicon.ico', sizes: 'any' },
       { url: '/papex-favicon.ico', sizes: 'any' },
-      { url: '/favicon.png', sizes: 'any' },
-      { url: '/papex-favicon.png', sizes: 'any' }
-    ]
+      { url: '/papex-favicon.png', type: 'image/png' }
+    ],
+    shortcut: [{ url: '/papex-favicon.ico' }],
+    apple: [{ url: '/apple-icon.png' }],
+    other: [
+      {
+        rel: 'apple-touch-icon',
+        url: '/apple-icon.png',
+      },
+    ],
   }
 }
 
@@ -24,10 +30,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${barlow.variable} ${gloock.variable}`}>
       <head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" href="/papex-favicon.ico" />
-        <link rel="icon" href="/favicon.png" type="image/png" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" href="/papex-favicon.ico" sizes="any" />
+        <link rel="icon" href="/papex-favicon.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/apple-icon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className="font-barlow">{children}</body>
